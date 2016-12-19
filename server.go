073ed7276b1ff02644ad07d351c13c8410ccf320
Halfwit <michaelmisch1985@gmail.com>
2016-12-19 01:15:52 -0700

    Set up initial logging to named file, using directory configs for now

--- server.go
+++ server.go
@@ -3,60 +3,74 @@ package main
 import (
 	"crypto/tls"
 	"fmt"
+	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/thoj/go-ircevent"
 	"github.com/vaughan0/go-ini"
 )
 
+type Conf struct {
+	Server    string
+	UseTLS    string
+	Nick      string
+	User      string
+	Channels  string
+	Directory string
+}
+
+func writeFile(c *Conf, e *irc.Event) {
+	path := filepath.Join(c.Directory, e.Arguments[0])
+	if e.Arguments[0] == c.User {
+		path = filepath.Join(c.Directory, e.Nick)
+	}
+	f, err := os.OpenFile(path, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil {
+		fmt.Printf("Err %s", err)
+	}
+	defer f.Close()
+	f.WriteString(e.Raw)
+	f.WriteString("\n")
+}
+
 func setupServer(conf ini.File, section string) *irc.Connection {
 
-	server, _ := conf.Get(section, "Server")
-	usetls, _ := conf.Get(section, "UseTLS")
+	c := new(Conf)
+	c.Server, _ = conf.Get(section, "Server")
+	c.UseTLS, _ = conf.Get(section, "UseTLS")
+	c.Nick, _ = conf.Get(section, "Nick")
+	c.User, _ = conf.Get(section, "User")
+	c.Channels, _ = conf.Get(section, "Channels")
+	c.Directory, _ = conf.Get(section, "Directory")
 
-	nick, ok := conf.Get(section, "Nick")
-	if !ok {
-		fmt.Printf("Err: no Nick in section %s", server)
-		return nil
-	}
-	user, ok := conf.Get(section, "User")
-	if !ok {
-		fmt.Printf("Err: no User in section %s", server)
-		return nil
-	}
-	channels, ok := conf.Get(section, "Channels")
-	if !ok {
-		fmt.Printf("Err no Channels in section %s", server)
-		return nil
-	}
-//TODO: --debug
-//irccon.Debug = true
-//TODO: -v --verbose
-//irccon.VerboseCallbackHandler = true
-	irccon := irc.IRC(nick, user)
-	if usetls == "true" {
+	err := os.MkdirAll(c.Directory, 0744)
+
+	irccon := irc.IRC(c.Nick, c.User)
+	//TODO: Flag out
+	irccon.Debug = true
+	irccon.VerboseCallbackHandler = true
+
+	if c.UseTLS == "true" {
 		irccon.UseTLS = true
 		irccon.TLSConfig = &tls.Config{InsecureSkipVerify: true}
 	}
 	irccon.AddCallback("001", func(e *irc.Event) {
-		for _, channel := range strings.Split(channels, ", ") {
+		for _, channel := range strings.Split(c.Channels, ", ") {
 			irccon.Join(channel)
 		}
 	})
 	irccon.AddCallback("PRIVMSG", func(e *irc.Event) {
-/*TODO: Call writes to filesystem, etc from here */
-//If server dir doesn't exist, create dir
-//Create or append to files
-//e.Nick is name of personsaying the thing
-//Arguments[0] is channel
-//Arguments[1] is message
-		fmt.Printf("%s %s\n", e.Nick, e.Arguments[1])
+		writeFile(c, e)
 	})
 	irccon.AddCallback("CTCP_ACTION", func(e *irc.Event) {
-		fmt.Printf(" * %s %s\n", e.Nick, e.Arguments[1])
+		writeFile(c, e)
+	})
+	irccon.AddCallback("TOPIC", func(e *irc.Event) {
+		writeFile(c, e)
 	})
 	irccon.AddCallback("366", func(e *irc.Event) {})
-	err := irccon.Connect(server)
+	err = irccon.Connect(c.Server)
 	if err != nil {
 		fmt.Printf("Err %s", err)
 		return nil
