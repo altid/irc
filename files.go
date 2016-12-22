4700f8c2403976af5155793be860d490a4a7ad47
Halfwit <michaelmisch1985@gmail.com>
2016-12-22 00:34:44 -0700

    We have files working

--- files.go
+++ files.go
@@ -4,23 +4,44 @@ import (
 	"github.com/vaughan0/go-ini"
 )
 
-func setupShow(conf ini.File, section string, srv *server) *show {
+func setupShow(conf ini.File, section string, srv *Server) *show {
+
+	//TODO:srv.AddFile("main", current.buf)
+	srv.AddFile("main", "test")
+	//TODO:srv.AddFile("main", completionList)
+	srv.AddFile("ctl", "test")
 	show := new(show)
 	for key, value := range conf[section] {
 		switch key {
 		case "Title":
 			show.Title = (value == "show")
+			//TODO: srv.AddFile("title", usefultitle)
+			srv.AddFile("title", "ubqt-irc")
 		case "Status":
 			show.Status = (value == "show")
+			//TODO: srv.AddFile("status", mode/buffer/etc)
+			srv.AddFile("status", "test")
 		case "Tabs":
 			show.Tabs = (value == "show")
+			//TODO: srv.AddFile("tabs", bufferlist)
+			srv.AddFile("tabs", "test")
 		case "Input":
 			show.Input = (value == "show")
+			srv.AddFile("input", "")
 		case "Sidebar":
 			show.Sidebar = (value == "show")
+			//TODO: srv.Addfile("sidebar", nicklist)
+			srv.AddFile("sidebar", "test")
 		case "Timestamps":
 			show.Timestamps = (value == "show")
 		}
 	}
 	return show
+}
+
+func (srv *Server) AddFile(key string, file interface{}) {
+	if srv.file == nil {
+		srv.file = make(map[string]interface{})
+	}
+	srv.file[key] = file
 }
--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
+	"log"
 	"os"
+	"path"
+	"strings"
 
+	"aqwari.net/net/styx"
 	"github.com/thoj/go-ircevent"
 	"github.com/vaughan0/go-ini"
 )
@@ -25,7 +30,7 @@ type show struct {
 	Timestamps bool
 }
 
-type server struct {
+type Server struct {
 	file map[string]interface{}
 }
 
@@ -35,13 +40,13 @@ func main() {
 		flag.Usage()
 		os.Exit(1)
 	}
+	var srv Server
 	conf, err := ini.LoadFile("irc.ini")
 	if err != nil {
 		fmt.Printf("Err %s", err)
 		return
 	}
 	irccon := make([]irc.Connection, 1)
-	var srv server
 	show := new(show)
 	for section, _ := range conf {
 		if section == "options" {
@@ -50,5 +55,77 @@ func main() {
 		}
 		irccon = append(irccon, *setupServer(conf, section))
 	}
+	var styxServer styx.Server
+	if *verbose {
+		styxServer.ErrorLog = log.New(os.Stderr, "",0)
+	}
+	if *debug {
+		styxServer.TraceLog = log.New(os.Stderr, "", 0)
+	}
+	styxServer.Addr = *addr
+	styxServer.Handler = &srv
 	fmt.Printf("Defaults\nShow title: %v\nShow tabs: %v\nShow status: %v\nShow input: %v\nShow sidebar: %v\n", show.Title, show.Tabs, show.Status, show.Input, show.Sidebar)
+	log.Fatal(styxServer.ListenAndServe())
+}
+
+func walkTo(v interface{}, loc string) (interface{}, bool){
+	cwd := v
+	parts := strings.FieldsFunc(loc, func(r rune) bool { return r == '/' })
+
+	for _, p := range parts {
+		switch v := cwd.(type) {
+		case map[string]interface{}:
+			child, ok := v[p]
+			if !ok {
+				return nil, false
+			} else {
+				cwd = child
+			}
+		default:
+			return nil, false
+		}
+	}
+	return cwd, true
+}
+
+func (srv *Server) Serve9P(s *styx.Session) {
+	for s.Next() {
+		t := s.Request()
+		file, ok := walkTo(srv.file, t.Path())
+		if !ok {
+			t.Rerror("no such file or directory")
+			continue
+		}
+		fi := &stat{name: path.Base(t.Path()), file: &fakefile{v: file}}
+		switch t:= t.(type) {
+		case styx.Twalk:
+			t.Rwalk(fi, nil)
+		case styx.Topen:
+			switch v := file.(type) {
+			case map[string]interface{}:
+				t.Ropen(mkdir(v), nil)
+			default:
+				t.Ropen(strings.NewReader(fmt.Sprint(v)), nil)
+			}
+		case styx.Tstat:
+			t.Rstat(fi, nil)
+		case styx.Tcreate:
+			switch v := file.(type) {
+			case map[string]interface{}:
+				if t.Mode.IsDir(){
+					dir := make(map[string]interface{})
+					v[t.Name] = dir
+					t.Rcreate(mkdir(dir), nil)
+				} else {
+					v[t.Name] = new(bytes.Buffer)
+					t.Rcreate(&fakefile{
+						v: v[t.Name],
+						set: func(s string) { v[t.Name] = s},
+					}, nil)
+				}
+			default:
+				t.Rerror("%s is not a directory", t.Path())
+			}
+		}
+	}
 }
