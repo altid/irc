238a581821ece333ec0100a1994cced944c62a27
Halfwit <michaelmisch1985@gmail.com>
2017-01-30 11:08:25 -0700

    Changing to using ubqtlib, will try out new irc lib as well

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/thoj/go-ircevent"
+	"github.com/ubqt-systems/ubqtlib"
 )
 
 var (
@@ -16,46 +16,23 @@ var (
 )
 
 //TODO: Switch to lstanley/girc instead, it's still event based but much more mature, with better tracking
-//TODO: Abstract back to a library
-// ubqt type
-// addr, inpath, debug, verbose are used to set up
-// ubqt.Start() afterwards
-// ubqt.NewSession(), defaults for addr inPath, debug, verbose
-// ubqt.ListenAndServe() could be a dummy wrapper too.
-// See how to make these available as functions to anything
-// that satisfies our interface
-// Special filename input, ctl, etc
-// type ubqt interface {
-//     ReadFile(filename string) ([]byte, error)
-//     WriteFile(filename string, data []byte, perm os.FileMode)  error
-//}
-// Have an enum of our explicit file names, mapped to strings
-// select {
-// case UB_INPUT
-//   - etc
-// default:
-//   - check if file exists (get path)
-// So it'd be implemented as a reader/writer/closer, and then we just handle it
-// Maybe ReadFile/WriteFile/CloseFile
-// ReadFile(b []byte, f *File)
-// Implement functions, 9p handles the rest.
-// If no data == nil, don't draw file
 
 // State - holds server session
 type State struct {
-	show   map[string]bool
-	irc    map[string]*irc.Connection
-	event  chan string
-	done   chan int
 	buffer string
 	server string
-	// This data is shared across all client sessions
-	// input for history of entered commands, this is across all channels
-	//TODO: input history: shared or per client
-	input []byte
-	// Tabs will include the current, plus all with activity
-	tabs []byte
-	ctl  string
+}
+
+func (st *State) WriteFile(filename string, data []byte, perm os.FileMode) error {
+	return nil
+}
+
+func (st *State) ReadFile(filename string) ([]byte, error) {
+	return []byte("Hello world\n"), nil
+}
+
+func (st *State) CloseFile(filename string) error {
+	return nil
 }
 
 func main() {
@@ -65,14 +42,13 @@ func main() {
 		os.Exit(1)
 	}
 	st := &State{}
-	err := st.Initialize()
+	srv := ubqtlib.NewSrv()
+	err := st.initialize(srv)
 	if err != nil {
 		fmt.Printf("Err %s", err)
 		os.Exit(1)
 	}
-	// Update data, write to IRC
-	go inputHandler(st)
-	err = st.Run()
+	err = srv.Loop(st)
 	if err != nil {
 		fmt.Printf("Err %s", err)
 		os.Exit(1)
--- state.go
+++ state.go
@@ -1,45 +1,33 @@
 package main
 
 import (
+	"github.com/ubqt-systems/ubqtlib"
 	"github.com/vaughan0/go-ini"
 )
 
-func parseOptions(st *State, conf ini.File) {
-	st.show = make(map[string]bool)
-	st.event = make(chan string)
-	st.done = make(chan int)
+func parseOptions(srv *ubqtlib.Srv, conf ini.File) {
 	for key, value := range conf["options"] {
-		switch key {
-		case "Title":
-			st.show["title"] = (value == "show")
-		case "Status":
-			st.show["status"] = (value == "show")
-		case "Tabs":
-			st.show["tabs"] = (value == "show")
-		case "Input":
-			st.show["input"] = (value == "show")
-		case "Sidebar":
-			st.show["sidebar"] = (value == "show")
+		if value == "show" {
+			srv.AddFile(key)
 		}
 	}
 }
 
 // Initialize - Read config and set up IRC sessions per entry
-func (st *State) Initialize() error {
+func (st *State) initialize(srv *ubqtlib.Srv) error {
 	//st.ctl = getCtl()
 	conf, err := ini.LoadFile("irc.ini")
 	if err != nil {
 		return err
 	}
-	parseOptions(st, conf)
-	st.show["ctl"] = true
-	st.show["main"] = true
+	parseOptions(srv, conf)
+	srv.AddFile("ctl")
+	srv.AddFile("main")
 	for section := range conf {
 		if section == "options" {
 			continue
 		}
 		// Fires off IRC sessions
-		setupServer(conf, section, st)
 		st.buffer = section
 		st.server = section
 	}
