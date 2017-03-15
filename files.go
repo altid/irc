ae8d33415b24681a3e40a80bc820edf52d9d124f
Halfwit <michaelmisch1985@gmail.com>
2017-03-14 23:50:29 -0600

    Switching to tailing

--- files.go
+++ files.go
@@ -12,21 +12,5 @@ func (st *State) writeServer(c *girc.Client, e girc.Event) {
 }
 
 func (st *State) writeChannel(c *girc.Client, e girc.Event) {
-	go st.wait("main")
 	fmt.Println(string(e.Bytes()))
-}
-
-func (st *State) wait(title string) {
-	switch title {
-	case "title":
-		<-st.titlch
-	case "main":
-		<-st.buffch
-	case "tabs":
-		<-st.tabsch
-	case "status":
-		<-st.statch
-	case "sidebar":
-		<-st.sidech
-	}
 }
