6815ad2ac5855853ac1b718d761d31594e096e76
Halfwit <michaelmisch1985@gmail.com>
2018-05-15 21:53:31 -0600

    Currently broken, but need to stash before bed
    Signed-off-by: Halfwit <michaelmisch1985@gmail.com>

--- files.go
+++ files.go
@@ -23,6 +23,8 @@ func (st *State) join(c *girc.Client, e girc.Event) {
 	// TODO: Add other user to map[username]timestamp, for Smart filters
 	if c.GetNick() == e.Source.Name {
 		go InitBuffer(c, e)
+	} else {
+		st.SendEvent(path.Join(c.Config.Server, "feed"))
 	}
 }
 
@@ -41,10 +43,16 @@ func InitBuffer(c *girc.Client, e girc.Event) {
 	//joined.UserList - list of all members
 	// TODO: Loop through joined.Admins and joined.Trusted, add them to the file at the top - and remove the named entries from UserList
 	// Finally, add all entries left from UserList
+	// Send events for all files updated here
+	st.SendEvent(path.Join(c.Config.Server, "status"))
+	st.SendEvent(path.Join(c.Config.Server, "title"))
+	st.SendEvent(path.Join(c.Config.Server, "feed"))
+	//st.SendEvent(path.Join(c.Config.Server, "sidebar"))
+	//st.SendEvent(path.Join(c.Config.Server, "tabs"))
 }
 
 func writeFile(m *message, fp string, format *template.Template) {
-	f, err := os.OpenFile(fp, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0666)
+	f, err := os.OpenFile(path.Join(*inPath, fp), os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0666)
 	defer f.Close()
 	if err != nil {
 		fmt.Println(err)
@@ -58,6 +66,7 @@ func writeFile(m *message, fp string, format *template.Template) {
 		fmt.Println(err)
 	}
 	fmt.Fprint(f, "\n")
+	st.SendEVent(path.Join(c.Config.Server, "feed")
 }
 
 func (st *State) writeFeed(c *girc.Client, e girc.Event) {
@@ -72,23 +81,23 @@ func (st *State) writeFeed(c *girc.Client, e girc.Event) {
 		} else {
 			name = c.Config.Server
 		}
-		filePath := path.Join(*inPath, c.Config.Server, "server", "feed")
+		filePath := path.Join(c.Config.Server, "server", "feed")
 		writeFile(&message{Name: name, Data: e.Trailing}, filePath, st.ntfyFmt) 
 	case "MODE":
-		filePath := path.Join(*inPath, c.Config.Server, "server", "feed")
+		filePath := path.Join(c.Config.Server, "server", "feed")
 		writeFile(&message{Name: c.Config.Server, Data: e.Trailing}, filePath, st.chanFmt)
 	case "PRIVMSG":
 		name := e.Params[0]
 		format := st.chanFmt
 		data := e.Trailing
 		nick := c.GetNick()
-		filePath := path.Join(*inPath, c.Config.Server, e.Params[0], "feed")
+		filePath := path.Join(c.Config.Server, e.Params[0], "feed")
 		if e.IsFromUser() {
 			// Assure we create the directory
 			name = e.Source.Name
-			dir := path.Join(*inPath, c.Config.Server, "~" + e.Source.Name)
+			dir := path.Join(c.Config.Server, "~" + e.Source.Name)
 			filePath = path.Join(dir, "feed")
-			os.MkdirAll(dir, 0777)
+			os.MkdirAll(path.Join(*inPath, dir), 0777)
 		}
 		if e.IsFromChannel() {
 			if strings.Contains(e.Trailing, nick) {
@@ -107,7 +116,7 @@ func (st *State) writeFeed(c *girc.Client, e girc.Event) {
 
 // Run through formatter and output to irc.freenode.net/server for example 
 func (st *State) writeServer(c *girc.Client, e girc.Event) {
-	filePath := path.Join(*inPath, c.Config.Server, "server", "feed")
+	filePath := path.Join(c.Config.Server, "server", "feed")
 	writeFile(&message{Name: "Server", Data: e.Trailing}, filePath, st.chanFmt) 
 }
 
@@ -124,7 +133,7 @@ func (st *State) mode(c *girc.Client, e girc.Event) {
 	if parnum == 1 {
 		return
 	}
-	filePath := path.Join(*inPath, c.Config.Server, e.Params[0])
+	filePath := path.Join(c.Config.Server, e.Params[0])
 	writeFile(&message{Name: e.Source.Name, Data: strings.Join(e.Params[1:], " ")}, path.Join(filePath, "feed"), st.modeFmt)
 }
 
@@ -135,10 +144,10 @@ func (st *State) quitServer(c *girc.Client, e girc.Event) {
 
 // Log to channel and update `title` file.
 func (st *State) topic(c *girc.Client, e girc.Event) {
-	filePath := path.Join(*inPath, c.Config.Server, e.Params[0])
+	filePath := path.Join(c.Config.Server, e.Params[0])
 	writeFile(&message{Name: e.Source.Name, Data: "has changed the topic to \"" + e.Trailing + "\""}, path.Join(filePath, "feed"), st.chanFmt) 
 	data := cleanmark.CleanString(e.Trailing)
-	ioutil.WriteFile(path.Join(filePath, "title"), []byte(data), 0666)
+	ioutil.WriteFile(path.Join(*inPath, filePath, "title"), []byte(data), 0666)
 }
 
 func (st *State) InLoop() {
