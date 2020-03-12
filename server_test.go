be6312fa79b9555276b6eff48a365f39975521a6
halfwit <michaelmisch1985@gmail.com>
2020-03-12 12:50:13 -0700

    (Broken until libs/fs PR complete) add tests for input

---
+++ server_test.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"context"
+	"testing"
+
+	"github.com/altid/libs/fs"
+)
+
+func TestCmds(t *testing.T) {
+	s := &server{
+		i:     make(chan string),
+		e:     make(chan string),
+		j:     make(chan string),
+		m:     make(chan *msg),
+		debug: func(ctlItem, ...interface{}) {},
+	}
+
+	reqs := make(chan string)
+
+	mcf, err := fs.MockCtlFile(s, reqs, false)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	ctx := context.Background()
+	defer ctx.Done()
+	defer mcf.Cleanup()
+
+	go s.fileListener(ctx, mcf)
+	go runCommands(reqs)
+
+	if e := mcf.Listen(); e != nil {
+		t.Error(err)
+	}
+}
+
+func runCommands(reqs chan string) {
+	reqs <- "open foo"
+	reqs <- "open bar"
+	reqs <- "join foo"
+	reqs <- "join bar"
+	reqs <- "part foo"
+	reqs <- "me bar eats chili"
+	reqs <- "quit"
+}
+
+func TestServerInput(t *testing.T) {
+	s := &server{
+		i:     make(chan string),
+		e:     make(chan string),
+		j:     make(chan string),
+		m:     make(chan *msg),
+		debug: func(ctlItem, ...interface{}) {},
+	}
+
+	reqs := make(chan string)
+
+	mcf, err := fs.MockCtlFile(s, reqs, false)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	ctx := context.Background()
+	defer ctx.Done()
+	defer mcf.Cleanup()
+
+	go s.fileListener(ctx, mcf)
+
+	go func() {
+		mcf.CreateBuffer("foo", "feed")
+		in := make(chan string)
+
+		input, err := fs.NewMockInput(s, "foo", false, in)
+		if err != nil {
+			t.Error(err)
+		}
+
+		input.Start()
+		in <- "test some"
+		in <- "input"
+		in <- "make some"
+		in <- "things break"
+		in <- "invalid-tokens"
+
+		if e := input.Errs(); len(e) > 0 {
+			for _, err := range e {
+				t.Error(err)
+			}
+		}
+	}()
+
+	if e := mcf.Listen(); e != nil {
+		t.Error(err)
+	}
+}
