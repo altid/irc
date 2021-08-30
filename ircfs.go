3c9d1dcbe86077c443cb4688acd26322c9de3101
Michael Misch <michealmisch1985@gmail.com>
2021-08-29 19:21:10 -0700

    Note what needs to be done after server libs are ready to go

--- ircfs.go
+++ ircfs.go
@@ -1,9 +1,15 @@
 package ircfs
 
 // This connects to IRC, manages interactions with the plugins 
-
 func Register(name string, server.Server) {
-
+    // Call fs library register function
+    // do our initialization, setup if mdns/regsitry
+    // Connect to IRC, start session
 }
 
 // network.Connect
+// Set up Defaults for session here
+// marshall our config with flagged sent in from cmd
+// set up server
+// create fs ctrl
+// create buffer, listener, file listener, connect, etc
